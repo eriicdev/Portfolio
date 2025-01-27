c833c884e3f4bf414c31705dc47085ebea6d1192
Erik costa <ec5489976@gmail.com>
2025-01-27 11:26:01 -0300

    Criando os componentes para titulos e paragrafos

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from "../../components/Titulo";
+
+const Sidebar = () => (
+    <aside>
+        <img src="https://github.com/eriicdev.png"/>
+        <Titulo fontSize={20}>Erik Costa</Titulo>
+    </aside>
+)
+
+export default Sidebar;
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,13 @@
+import Titulo from "../../components/Titulo";
+import Paragrafo from "../../components/Paragrafo";
+
+const Sobre = () => (
+    <section>
+        <Titulo fontSize={16}>Sobre mim</Titulo>
+        <Paragrafo tipo ="principal">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi repudiandae amet quod saepe similique. Modi, eveniet quos dicta, ipsam, laborum sequi numquam obcaecati consectetur exercitationem atque provident repudiandae a tempore.
+        </Paragrafo>
+    </section>
+)
+
+export default Sobre;
